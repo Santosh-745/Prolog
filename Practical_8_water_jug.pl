33f4a883036ef658ebc9972e409706b74ae0a2aa
Santosh1652 <80413971+Santosh1652@users.noreply.github.com>
2021-03-11 12:37:05 +0530

    Update Practical_8_water_jug.pl

--- Practical_8_water_jug.pl
+++ Practical_8_water_jug.pl
@@ -1,59 +1,49 @@
 apend([],L,L).
 apend([X|L1],L2,[X|L3]):-
-     append(L1,L2,L3).
+     apend(L1,L2,L3).
 
 ismem(X,[X|_]).
 ismem(X,[_|Y]):-
      ismem(X,Y).
 
 
-fun([X,Y],_,_,_):-
-    ( X=4 ; Y=4  ),
+fun([X,Y],_,_,G,_):-
+    ( X = G ; Y= G ),
+    write("\n"),
+    write([X,Y]),
+    write("\t to \t"),
+    write([G,0]),
+    write("\n"),
     write("goal state reached").
 
-
-fun([X,Y],M,N,L):-
+fun([X,Y],M,N,G,L):-
     Y < N,
     \+ismem([X,N],L),
     apend(L,[[X,N]],L1),
     write("\n"),
+    write("Rule 2 "),
+    write("\n"),
     write([X,Y]),
     write("\t to \t"),
     write([X,N]),
-    fun([X,N],M,N,L1).
+    fun([X,N],M,N,G,L1).
 
-fun([X,Y],M,N,L):-
-    X < M,
-    \+ismem([M,Y],L),
-    apend(L,[[M,Y]],L1),
-    write("\n"),
-    write([X,Y]),
-    write("\t to \t"),
-    write([M,Y]),
-    fun([M,Y],M,N,L1).
-
-fun([X,Y],M,N,L):-
-    X > 0,
-    \+ismem([0,Y],L),
-    apend(L,[[0,Y]],L1),
-    write("\n"),
-    write([X,Y]),
-    write("\t to \t"),
-    write([0,Y]),
-    fun([0,Y],M,N,L1).
-
-fun([X,Y],M,N,L):-
+fun([X,Y],M,N,G,L):-
+    Y1 is X + Y,
+    Y1 > 0,
+    Y1 =< 4,
     Y > 0,
-    \+ismem([X,0],L),
-    apend(L,[[X,0]],L1),
+    \+ismem([Y1,0],L),
+    apend(L,[[Y1,0]],L1),
+    write("\n"),
+    write("Rule 7"),
     write("\n"),
     write([X,Y]),
     write("\t to \t"),
-    write([X,0]),
-    fun([X,0],M,N,L1).
+    write([Y1,0]),
+    fun([Y1,0],M,N,G,L1).
 
-
-fun([X,Y],M,N,L):-
+fun([X,Y],M,N,G,L):-
     Y1 is X + Y,
     Y1 > 0,
     Y1 >= M,
@@ -63,12 +53,50 @@ fun([X,Y],M,N,L):-
     \+ismem([M,Y2],L),
     apend(L,[[M,Y2]],L1),
     write("\n"),
+    write("Rule 5"),
+    write("\n"),
     write([X,Y]),
     write("\t to \t"),
     write([M,Y2]),
-    fun([M,Y2],M,N,L1).
+    fun([M,Y2],M,N,G,L1).
 
-fun([X,Y],M,N,L):-
+fun([X,Y],M,N,G,L):-
+    X < M,
+    \+ismem([M,Y],L),
+    apend(L,[[M,Y]],L1),
+    write("\n"),
+    write("Rule 1"),
+    write("\n"),
+    write([X,Y]),
+    write("\t to \t"),
+    write([M,Y]),
+    fun([M,Y],M,N,G,L1).
+
+fun([X,Y],M,N,G,L):-
+    X > 0,
+    \+ismem([0,Y],L),
+    apend(L,[[0,Y]],L1),
+    write("\n"),
+    write("Rule 3"),
+    write("\n"),
+    write([X,Y]),
+    write("\t to \t"),
+    write([0,Y]),
+    fun([0,Y],M,N,G,L1).
+
+fun([X,Y],M,N,G,L):-
+    Y > 0,
+    \+ismem([X,0],L),
+    apend(L,[[X,0]],L1),
+    write("\n"),
+    write("Rule 4"),
+    write("\n"),
+    write([X,Y]),
+    write("\t to \t"),
+    write([X,0]),
+    fun([X,0],M,N,G,L1).
+
+fun([X,Y],M,N,G,L):-
     Y1 is X + Y,
     Y1 > 0,
     Y1 >= N,
@@ -78,27 +106,14 @@ fun([X,Y],M,N,L):-
     \+ismem([Y2,N],L),
     apend(L,[[Y2,N]],L1),
     write("\n"),
-    write([X,Y]),
-    write("\t to \t"),
-    write([Y2,N]),
-    fun([Y2,N],M,N,L1).
-
-
-fun([X,Y],M,N,L):-
-    Y1 is X + Y,
-    Y1 > 0,
-    Y1 =< 4,
-    Y > 0,
-    \+ismem([Y1,0],L),
-    apend(L,[[Y1,0]],L1),
+    write("Rule 6"),
     write("\n"),
     write([X,Y]),
     write("\t to \t"),
-    write([Y1,0]),
-    fun([Y1,0],M,N,L1).
+    write([Y2,N]),
+    fun([Y2,N],M,N,G,L1).
 
-
-fun([X,Y],M,N,L):-
+fun([X,Y],M,N,G,L):-
     Y1 is X + Y,
     Y1 > 0,
     Y1 =< N,
@@ -106,19 +121,9 @@ fun([X,Y],M,N,L):-
     \+ismem([0,Y1],L),
     apend(L,[[0,Y1]],L1),
     write("\n"),
+    write("Rule 8"),
+    write("\n"),
     write([X,Y]),
     write("\t to \t"),
     write([0,Y1]),
-    fun([0,Y1],M,N,L1).
-
-
-
-
-
-
-
-
-
-
-
-
+    fun([0,Y1],M,N,G,L1).
